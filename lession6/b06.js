0156c03f664fc9957ea0d45690b8a1cef8e4f8dc
Đỗ Khắc Quyền <quyendk.b24dtcn380@stu.ptit.edu.vn>
2025-03-21 10:47:03 +0700

    Update b06.js

--- lession6/b06.js
+++ lession6/b06.js
@@ -1,13 +1,13 @@
-let size = 20;
+let size = 40;
 
 document.getElementById("cong").addEventListener("click", function () {
   const text = document.getElementById("text");
-  size += 5;
+  size += 4;
   text.style.fontSize = String(size) + "px";
 });
 
 document.getElementById("tru").onclick = () => {
   const text = document.getElementById("text");
-  size -= 5;
+  size -= 4;
   text.style.fontSize = String(size) + "px";
 };
